7cf79a45ac4fc68f0a9ecf23a2e58eb605c94707
Ian Effendi <effendiian+github@gmail.com>
2016-04-17 12:19:57 -0400

    Overview: Intergrated the template files provided by professor for extending the program.

---
+++ work/Homework 4__DoublyLinkedLists/Homework 4__DoublyLinkedLists/IList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4__DoublyLinkedLists
+{
+
+	// < THIS CLASS WAS PROVIDED AS-IS BY THE PROFESSOR
+	// AS PART OF THE ASSIGNMENT. >
+
+	/// <summary>
+	/// The interface for a simple Linked List
+	/// </summary>
+	public interface IList
+	{
+		// Add a new element to the end of the list.
+		void Add(string data);
+
+		// Inserts a new element at a specified location in the list.
+		void Insert(string data, int index);
+
+		// Remove an element from a specified location in 
+		// the list and return the data
+		string Remove(int index);
+
+		// Return the element at this index in the list.
+		string GetElement(int index);
+
+		// Clears the list
+		void Clear();
+
+		// The current number of items in this list.
+		int Count { get; }
+
+	}
+}
---
+++ work/Homework 4__DoublyLinkedLists/Homework 4__DoublyLinkedLists/Node.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4__DoublyLinkedLists
+{
+	// < THIS CLASS WAS PROVIDED AS-IS BY THE PROFESSOR
+	// AS PART OF THE ASSIGNMENT. >
+
+	/// <summary>
+	/// Represents one node in a Linked List
+	/// </summary>
+	public class Node
+	{
+		// Attributes
+		private string data;    // The data in this node
+		private Node next;  // The next node in the list
+		private Node previous;  // The previous node in the list
+
+		/// <summary>
+		/// Gets and sets the data of this node
+		/// </summary>
+		public string Data
+		{
+			get { return data; }
+			set { data = value; }
+		}
+
+		/// <summary>
+		/// Gets and sets the next node
+		/// </summary>
+		public Node Next
+		{
+			get { return next; }
+			set { next = value; }
+		}
+
+		/// <summary>
+		/// Gets and sets the previous node
+		/// </summary>
+		public Node Previous
+		{
+			get { return previous; }
+			set { previous = value; }
+		}
+
+		/// <summary>
+		/// Creates a new Node with data
+		/// </summary>
+		/// <param name="data">The data to hold in this node</param>
+		public Node(string data)
+		{
+			this.data = data;
+			this.next = null;
+			this.previous = null;
+		}
+
+	}
+}
